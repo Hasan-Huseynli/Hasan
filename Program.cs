fb1c60eee1e747f2fb5acbd3b8509ae209ada36a
Hasan Huseynli <hasan@Hasans-MacBook-Pro.local>
2022-12-10 00:24:07 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+{
+    int a = 10;
+    int b = 15;
+    Console.WriteLine(a + b);
+    Console.ReadKey();
+}
+
